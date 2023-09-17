6a0137649bd53680325d0c60e641b0e23f21c7e5
Eduardo Scaranari <dev.edusc@gmail.com>
2023-09-17 15:29:54 -0300

    Landscape SubsScreen done.

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/presentation/subs/SubsComponent.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/presentation/subs/SubsComponent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -17,39 +18,54 @@ import androidx.compose.ui.unit.dp
 import com.duscaranari.themedbingocardsgenerator.R
 
 @Composable
-fun SubsPlanCards(
+fun SubsPlanCardsColumn(
     onClick: (planId: String) -> Unit
 ) {
 
-    Column {
+    val cardModifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 4.dp, horizontal = 16.dp)
 
-        SubsCard(
-            plan = stringResource(id = R.string.monthly_plan),
-            description = stringResource(id = R.string.monthly_plan_desc),
-            onClick = { onClick("monthly-drawer-access") }
-        )
+    LazyColumn {
 
-        SubsCard(
-            plan = stringResource(id = R.string.trimester_plan),
-            description = stringResource(id = R.string.trimester_plan_desc),
-            onClick = { onClick("trimestral-drawer-access") }
-        )
+        item {
+            SubsCard(
+                plan = stringResource(id = R.string.monthly_plan),
+                description = stringResource(id = R.string.monthly_plan_desc),
+                onClick = { onClick("monthly-drawer-access") },
+                cardModifier = cardModifier
+            )
+        }
 
-        Text(
-            text = stringResource(id = R.string.best_option),
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.padding(start = 16.dp, top = 16.dp)
-        )
+        item {
+            SubsCard(
+                plan = stringResource(id = R.string.trimester_plan),
+                description = stringResource(id = R.string.trimester_plan_desc),
+                onClick = { onClick("trimestral-drawer-access") },
+                cardModifier = cardModifier
+            )
+        }
 
-        SubsCard(
-            plan = stringResource(id = R.string.yearly_plan),
-            description = stringResource(id = R.string.yearly_plan_desc),
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary,
-            onClick = { onClick("annual-drawer-access") }
-        )
+        item {
+            Text(
+                text = stringResource(id = R.string.best_option),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+            )
+        }
+
+        item {
+            SubsCard(
+                plan = stringResource(id = R.string.yearly_plan),
+                description = stringResource(id = R.string.yearly_plan_desc),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                onClick = { onClick("annual-drawer-access") },
+                cardModifier = cardModifier
+            )
+        }
     }
 }
 
@@ -59,7 +75,8 @@ private fun SubsCard(
     description: String,
     containerColor: Color = MaterialTheme.colorScheme.primaryContainer,
     contentColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    cardModifier: Modifier
 ) {
 
     Card(
@@ -67,9 +84,7 @@ private fun SubsCard(
             containerColor = containerColor,
             contentColor = contentColor
         ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp, horizontal = 16.dp)
+        modifier = cardModifier
             .clickable { onClick() }
     ) {
         Column(
--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/presentation/subs/SubsScreen.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/presentation/subs/SubsScreen.kt
@@ -1,7 +1,6 @@
 package com.duscaranari.themedbingocardsgenerator.presentation.subs
 
 import android.app.Activity
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,14 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -25,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.duscaranari.themedbingocardsgenerator.R
+import com.duscaranari.themedbingocardsgenerator.ui.theme.LandscapePreviews
 import com.duscaranari.themedbingocardsgenerator.ui.theme.PortraitPreviews
 import com.duscaranari.themedbingocardsgenerator.util.BillingHelper
 import com.duscaranari.themedbingocardsgenerator.util.DeviceOrientation
@@ -35,28 +30,63 @@ fun SubsScreen(subsViewModel: SubsViewModel = hiltViewModel()) {
 
     val billingHelper = subsViewModel.billingClientSetup(LocalContext.current as Activity)
 
-    when (rememberDeviceOrientation()) {
-        is DeviceOrientation.Portrait -> PortraitSubsScreen(billingHelper)
-        else -> LandscapeSubsScreen(billingHelper)
-    }
-}
-
-@Composable
-fun PortraitSubsScreen(billingHelper: BillingHelper?) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
 
-        Column(
-            modifier = Modifier.sizeIn(
-                maxWidth = 600.dp,
-                maxHeight = 1000.dp
-            )
-        ) {
+        when (rememberDeviceOrientation()) {
+            is DeviceOrientation.Portrait -> PortraitSubsScreen(billingHelper)
+            else -> LandscapeSubsScreen(billingHelper)
+        }
+    }
 
-            Spacer(modifier = Modifier.weight(0.5f))
+}
 
+@Composable
+fun PortraitSubsScreen(billingHelper: BillingHelper) {
+
+    Column(
+        modifier = Modifier.sizeIn(
+            maxWidth = 600.dp,
+            maxHeight = 1000.dp
+        )
+    ) {
+
+        Spacer(modifier = Modifier.weight(0.5f))
+
+        Text(
+            text = stringResource(id = R.string.plan),
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.plan_desc),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        SubsPlanCardsColumn(
+            onClick = { billingHelper.checkSubscriptionStatus(it) }
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun LandscapeSubsScreen(billingHelper: BillingHelper) {
+
+    Row(
+        modifier = Modifier.sizeIn(
+            maxWidth = 1000.dp,
+            maxHeight = 600.dp
+        )
+    ) {
+
+        Column(modifier = Modifier.weight(0.3f)) {
             Text(
                 text = stringResource(id = R.string.plan),
                 style = MaterialTheme.typography.titleLarge,
@@ -67,26 +97,12 @@ fun PortraitSubsScreen(billingHelper: BillingHelper?) {
                 text = stringResource(id = R.string.plan_desc),
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
+        }
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            SubsPlanCards(
-                onClick = { billingHelper?.checkSubscriptionStatus(it) }
+        Column(Modifier.weight(0.7f)) {
+            SubsPlanCardsColumn(
+                onClick = { billingHelper.checkSubscriptionStatus(it) }
             )
-
-            Spacer(modifier = Modifier.weight(1f))
         }
     }
-
-}
-
-@Composable
-fun LandscapeSubsScreen(billingHelper: BillingHelper?) {
-
-}
-
-@PortraitPreviews
-@Composable
-fun PortraitSubsScreenPreview() {
-    PortraitSubsScreen(null)
 }
