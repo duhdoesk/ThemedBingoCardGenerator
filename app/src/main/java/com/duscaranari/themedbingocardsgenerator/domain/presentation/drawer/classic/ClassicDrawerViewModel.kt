80a4bad8229561c481a74a9024437fa1d9952df4
Eduardo Scaranari <dev.edusc@gmail.com>
2023-12-16 21:58:49 -0300

    created ClassicDrawRepository

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/domain/presentation/drawer/classic/ClassicDrawerViewModel.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/domain/presentation/drawer/classic/ClassicDrawerViewModel.kt
@@ -1,11 +1,13 @@
 package com.duscaranari.themedbingocardsgenerator.domain.presentation.drawer.classic
 
-import com.duscaranari.themedbingocardsgenerator.data.ClassicDrawDao
+import androidx.lifecycle.ViewModel
+import com.duscaranari.themedbingocardsgenerator.domain.repository.ClassicDrawRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
-class ClassicDrawerViewModel @Inject constructor(private val classicDrawDao: ClassicDrawDao) {
+class ClassicDrawerViewModel @Inject constructor(private val classicDrawRepository: ClassicDrawRepository)
+    : ViewModel() {
 
     private val _uiState = MutableStateFlow<ClassicDrawerUiState>(ClassicDrawerUiState.Loading)
     val uiState = _uiState.asStateFlow()
