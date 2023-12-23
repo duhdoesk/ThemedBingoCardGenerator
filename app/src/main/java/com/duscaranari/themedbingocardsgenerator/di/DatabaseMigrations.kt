ef7bb060506d67c673ed13cfed934f26520066c6
Eduardo Scaranari <dev.edusc@gmail.com>
2023-12-23 12:02:05 -0300

    fixed db migration

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/di/DatabaseMigrations.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/di/DatabaseMigrations.kt
@@ -8,6 +8,7 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         database.execSQL(
             "CREATE TABLE `classic_draw_table` (" +
                     "`draw_id` INTEGER NOT NULL, " +
+                    "`numbers` INTEGER NOT NULL, " +
                     "`is_completed` INTEGER NOT NULL, " +
                     "`drawn_numbers` TEXT NOT NULL, " +
                     "PRIMARY KEY(`draw_id`))")
