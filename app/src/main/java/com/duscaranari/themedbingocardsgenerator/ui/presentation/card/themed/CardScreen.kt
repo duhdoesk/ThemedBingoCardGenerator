db4d1622136a67a3f76123f2e12a7891760994c0
Eduardo Scaranari <dev.edusc@gmail.com>
2024-02-18 11:56:18 -0300

    LandscapeCardScreen progress.

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/card/themed/CardScreen.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/card/themed/CardScreen.kt
@@ -12,6 +12,7 @@ import com.duscaranari.themedbingocardsgenerator.ui.presentation.component.Loadi
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.themes.ThemesScreen
 import com.duscaranari.themedbingocardsgenerator.ui.navigation.AppScreens
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.card.themed.event.ThemedCardUiEvent
+import com.duscaranari.themedbingocardsgenerator.ui.presentation.card.themed.screens.landscape.NewLandscapeCardScreen
 import com.duscaranari.themedbingocardsgenerator.util.DeviceOrientation
 import com.duscaranari.themedbingocardsgenerator.util.rememberDeviceOrientation
 
@@ -39,16 +40,25 @@ fun CardScreen(
             when (rememberDeviceOrientation()) {
 
                 is DeviceOrientation.Landscape ->
-                    LandscapeCardScreen(
+                    NewLandscapeCardScreen(
                         state = state,
-                        onUpdateCurrentUser = { cardViewModel.updateCurrentUser(it) },
-                        onDrawNewCard = { cardViewModel.drawNewCard() },
-                        onNavToCharactersScreen = {
-                            navController.navigate(
-                                "${AppScreens.Character.name}/${state.currentTheme.themeId}"
-                            )
-                        },
-                        onChangeCardSize = { cardViewModel.onChangeCardSize(it) }
+                        event = { event ->
+                            when (event) {
+                                is ThemedCardUiEvent.OnChangeCardSize ->
+                                    cardViewModel.onChangeCardSize()
+
+                                is ThemedCardUiEvent.OnDrawNewCard ->
+                                    cardViewModel.drawNewCard()
+
+                                is ThemedCardUiEvent.OnUpdateCurrentUser ->
+                                    cardViewModel.updateCurrentUser(event.user)
+
+                                is ThemedCardUiEvent.OnNavigateToCharactersScreen ->
+                                    navController.navigate(
+                                        "${AppScreens.Character.name}/${state.currentTheme.themeId}"
+                                    )
+                            }
+                        }
                     )
 
                 else ->
---
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/card/themed/screens/landscape/MediumLandscapeCardScreen.kt
@@ -0,0 +1,13 @@
+package com.duscaranari.themedbingocardsgenerator.ui.presentation.card.themed.screens.landscape
+
+import androidx.compose.runtime.Composable
+import com.duscaranari.themedbingocardsgenerator.ui.presentation.card.themed.event.ThemedCardUiEvent
+import com.duscaranari.themedbingocardsgenerator.ui.presentation.card.themed.state.CardUiState
+
+@Composable
+fun MediumLandscapeCardScreen(
+    state: CardUiState.Success,
+    event: (event: ThemedCardUiEvent) -> Unit
+) {
+
+}
