b6a63709ef1595840999b7f0a80ddf06f6fac385
Eduardo Scaranari <dev.edusc@gmail.com>
2024-02-20 15:58:43 -0300

    fix interstitialAd was not being called.

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/home/HomeScreen.kt
@@ -1,11 +1,13 @@
 package com.duscaranari.themedbingocardsgenerator.ui.presentation.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.home.screens.component.BingoType
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.home.screens.landscape.LandscapeHomeScreen
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.home.screens.portrait.PortraitHomeScreen
 import com.duscaranari.themedbingocardsgenerator.util.DeviceOrientation
+import com.duscaranari.themedbingocardsgenerator.util.ads.showInterstitialAd
 import com.duscaranari.themedbingocardsgenerator.util.rememberDeviceOrientation
 
 @Composable
@@ -16,11 +18,17 @@ fun HomeScreen(
 ) {
 
     val bingoTypes = BingoType.getBingoTypes()
+    val context = LocalContext.current
 
     when (rememberDeviceOrientation()) {
         is DeviceOrientation.Portrait ->
             PortraitHomeScreen(
-                onNavigate = { navController.navigate(it) },
+                onNavigate = {
+                    navController.navigate(it)
+                    if (!subscribed) {
+                        showInterstitialAd(context)
+                    }
+                },
                 subscribed,
                 bingoTypes = bingoTypes,
                 onBingoTypeChange = { onBingoTypeChange(it) }
@@ -28,7 +36,12 @@ fun HomeScreen(
 
         else ->
             LandscapeHomeScreen(
-                onNavigate = { navController.navigate(it) },
+                onNavigate = {
+                    navController.navigate(it)
+                    if (!subscribed) {
+                        showInterstitialAd(context)
+                    }
+                },
                 subscribed,
                 bingoTypes = bingoTypes,
                 onBingoTypeChange = { onBingoTypeChange(it) }
