b970df270540243c68f529d49b0d38060c2f6050
Eduardo Scaranari <dev.edusc@gmail.com>
2024-03-31 14:27:41 -0300

    LandscapePlayerSessionScreen progress.

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/sessions/SessionsViewModel.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/sessions/SessionsViewModel.kt
@@ -3,12 +3,12 @@ package com.duscaranari.themedbingocardsgenerator.ui.presentation.sessions
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.duscaranari.themedbingocardsgenerator.R
+import com.duscaranari.themedbingocardsgenerator.data.network.firestore.model.ParticipantDTO
 import com.duscaranari.themedbingocardsgenerator.data.network.firestore.model.SessionDTO
-import com.duscaranari.themedbingocardsgenerator.domain.session.use_case.GetSessionsUseCase
 import com.duscaranari.themedbingocardsgenerator.domain.participant.use_case.JoinResult
 import com.duscaranari.themedbingocardsgenerator.domain.participant.use_case.JoinSessionUseCase
+import com.duscaranari.themedbingocardsgenerator.domain.session.use_case.GetNotStartedSessionsUseCase
 import com.duscaranari.themedbingocardsgenerator.domain.theme.use_case.GetAllBingoThemesUseCase
-import com.duscaranari.themedbingocardsgenerator.data.network.firestore.model.ParticipantDTO
 import com.duscaranari.themedbingocardsgenerator.util.auth.UserData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -17,12 +17,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SessionsViewModel @Inject constructor(
-    getSessionsUseCase: GetSessionsUseCase,
+    getNotStartedSessionsUseCase: GetNotStartedSessionsUseCase,
     private val joinSessionUseCase: JoinSessionUseCase,
     getAllBingoThemesUseCase: GetAllBingoThemesUseCase
 ) : ViewModel() {
 
-    private val _sessions = getSessionsUseCase.invoke()
+    private val _sessions = getNotStartedSessionsUseCase.invoke()
     val sessions = _sessions
         .stateIn(
             viewModelScope,
