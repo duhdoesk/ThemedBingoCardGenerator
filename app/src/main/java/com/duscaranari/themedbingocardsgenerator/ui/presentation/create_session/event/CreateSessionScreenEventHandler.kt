c060e65b5cae525ec9fc28588190f6cace493649
Eduardo Scaranari <dev.edusc@gmail.com>
2024-03-31 16:47:15 -0300

    fixed navigation for create new session.

--- app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/create_session/event/CreateSessionScreenEventHandler.kt
+++ app/src/main/java/com/duscaranari/themedbingocardsgenerator/ui/presentation/create_session/event/CreateSessionScreenEventHandler.kt
@@ -1,7 +1,5 @@
 package com.duscaranari.themedbingocardsgenerator.ui.presentation.create_session.event
 
-import android.content.Context
-import android.widget.Toast
 import androidx.navigation.NavController
 import com.duscaranari.themedbingocardsgenerator.ui.navigation.AppScreens
 import com.duscaranari.themedbingocardsgenerator.ui.presentation.create_session.CreateSessionViewModel
